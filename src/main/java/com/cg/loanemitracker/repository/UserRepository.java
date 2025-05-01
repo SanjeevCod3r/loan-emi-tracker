46a9b6ae73011ea1b66e33a014d799aecb648850
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-05-01 13:45:28 +0530

    [Sanjeev Jaiswal] Add: JWTAuthFilter UserDetailsServiceImp and UserRepo

---
+++ src/main/java/com/cg/loanemitracker/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.cg.loanemitracker.repository;
+
+
+import com.cg.loanemitracker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}

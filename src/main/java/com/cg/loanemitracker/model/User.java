90fd93d097e75376edb16ab345ef941365761f23
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-05-01 13:48:21 +0530

    [Nitesh] ADD: user model

--- src/main/java/com/cg/loanemitracker/model/User.java
+++ src/main/java/com/cg/loanemitracker/model/User.java
@@ -5,7 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -27,6 +32,11 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String email;
 
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return List.of();
+//    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singletonList(() -> "ROLE_USER");

1d930b33e27e2233796f08ea6e6a731eb5979402
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-05-01 09:56:50 +0530

    [Sanjeev Jaiswal] Add: Folder Structure Done

---
+++ src/main/java/com/cg/loanemitracker/LoanEmiTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.cg.loanemitracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEmiTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanEmiTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cg/loanemitracker/LoanEmiTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.loanemitracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEmiTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

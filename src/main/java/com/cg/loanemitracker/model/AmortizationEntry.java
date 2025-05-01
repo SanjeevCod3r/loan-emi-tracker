8cd769057b7b9d9b9ad67e525b5d6c9364c113e1
Dushyant1246 <dushyant.dhmn808@gmail.com>
2025-05-01 13:29:10 +0530

    [Dushyant] ADD: AmortizationEntry Model

--- src/main/java/com/cg/loanemitracker/model/AmortizationEntry.java
+++ src/main/java/com/cg/loanemitracker/model/AmortizationEntry.java
@@ -1,4 +1,38 @@
 package com.cg.loanemitracker.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "amortization_entries")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AmortizationEntry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private Integer monthNumber;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal principal;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal interest;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal remainingPrincipal;
+
+    @Column(nullable = false)
+    private LocalDate paymentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "loan_id", nullable = false)
+    private Loan loan;
 }
